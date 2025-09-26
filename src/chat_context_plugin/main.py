89e1d8fcf4caa13ba8ea4c5be258fe0953e55bba
lele <1789335298@qq.com>
2025-09-26 10:06:56 +0800

    add log

--- src/chat_context_plugin/main.py
+++ src/chat_context_plugin/main.py
@@ -216,7 +216,7 @@ class ChatContextPlugin(Plugin):
             nickname = message.contact.display_name
             try:
                 self.logger.info(f"群聊 [{session_name}] 用户 [{nickname}] 发送消息: {formatted_message['content']}")
-            except UnicodeEncodeError:
+            except BaseException:
                 self.logger.error("Decode Error")
             if session_name not in self.sessions_for_sheet:
                 self.sessions_for_sheet[session_name] = []
