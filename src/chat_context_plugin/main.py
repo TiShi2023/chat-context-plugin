035dc9673b4fc41ece6369f9abee6b6e6bea62fb
lele <1789335298@qq.com>
2025-09-26 09:59:57 +0800

    add log

--- src/chat_context_plugin/main.py
+++ src/chat_context_plugin/main.py
@@ -214,7 +214,10 @@ class ChatContextPlugin(Plugin):
         if message.is_chatroom:
             session_name = message.room.display_name
             nickname = message.contact.display_name
-            self.logger.info(f"群聊 [{session_name}] 用户 [{nickname}] 发送消息: {formatted_message['content']}")
+            try:
+                self.logger.info(f"群聊 [{session_name}] 用户 [{nickname}] 发送消息: {formatted_message['content']}")
+            except:
+                self.logger.error("Decode Error")
             if session_name not in self.sessions_for_sheet:
                 self.sessions_for_sheet[session_name] = []
             self.sessions_for_sheet[session_name].append((
